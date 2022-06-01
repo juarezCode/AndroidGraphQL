3222c41c3091585d5d02057346ea6459859e3d8c
Juarez Roberto <juarez.unknown@gmail.com>
2022-06-01 12:01:24 -0500

    :sparkles: first query

--- app/src/main/java/com/juarez/androidgraphql/LaunchListFragment.kt
+++ app/src/main/java/com/juarez/androidgraphql/LaunchListFragment.kt
@@ -1,10 +1,12 @@
 package com.juarez.androidgraphql
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.juarez.androidgraphql.databinding.FragmentLaunchListBinding
 
 class LaunchListFragment : Fragment() {
@@ -17,5 +19,15 @@ class LaunchListFragment : Fragment() {
     ): View {
         binding = FragmentLaunchListBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            val response = apolloClient.query(LaunchListQuery()).execute()
+            
+            Log.d("LaunchList", "Success ${response.data}")
+        }
     }
 }
