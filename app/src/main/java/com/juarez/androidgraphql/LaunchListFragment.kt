07fbe501fc6d32ca5eb77b24049148face200984
Juarez Roberto <juarez.unknown@gmail.com>
2022-06-01 12:12:28 -0500

    :sparkles: show launches in UI

--- app/src/main/java/com/juarez/androidgraphql/LaunchListFragment.kt
+++ app/src/main/java/com/juarez/androidgraphql/LaunchListFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apollographql.apollo3.exception.ApolloException
 import com.juarez.androidgraphql.databinding.FragmentLaunchListBinding
 
 class LaunchListFragment : Fragment() {
@@ -25,9 +27,20 @@ class LaunchListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launchWhenResumed {
-            val response = apolloClient.query(LaunchListQuery()).execute()
-            
-            Log.d("LaunchList", "Success ${response.data}")
+
+            val response = try {
+                apolloClient.query(LaunchListQuery()).execute()
+            } catch (e: ApolloException) {
+                Log.d("LaunchList", "Failure", e)
+                null
+            }
+
+            val launches = response?.data?.launches?.launches?.filterNotNull()
+            if (launches != null && !response.hasErrors()) {
+                val adapter = LaunchListAdapter(launches)
+                binding.launches.layoutManager = LinearLayoutManager(requireContext())
+                binding.launches.adapter = adapter
+            }
         }
     }
 }
